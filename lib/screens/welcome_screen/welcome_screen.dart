52c9ac0332a50a3ca555898fc694af0b0d5deccc
Maksym Dmytruk <dmitruk.max@gmail.com>
2025-03-14 01:58:53 +0200

    Create UI for WelcomeScreen

--- lib/screens/welcome_screen/welcome_screen.dart
+++ lib/screens/welcome_screen/welcome_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:memory_box/constants/icons_app/icons_app.dart';
 import 'package:memory_box/widgets/containers/container_info.dart';
 import 'package:memory_box/widgets/custom_background/custom_background.dart';
 
@@ -14,13 +16,27 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return CustomBackground(
       name: 'Ти супер!',
       child: Center(
-        child: ContainerInfo(
-          text: 'Ми раді тебе бачити',
-          fontSize: 24.0,
-          fontWeight: FontWeight.w400,
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: 51,
+          children: [
+            SizedBox(
+              height: size.height * 0.4,
+            ),
+            ContainerInfo(
+              text: 'Ми раді тебе бачити',
+              fontSize: 24.0,
+              fontWeight: FontWeight.w400,
+            ),
+            SvgPicture.asset(
+              IconsApp.heart,
+            ),
+          ],
         ),
       ),
     );
